cbb3d677eb875de77514fbd29b18a04aad857641
srebrek <zlotekmikolaj@gmail.com>
2025-08-15 01:37:33 +0200

    feat: enhance blazor app structure and functionality
    updated program.cs with new services and middleware, modified api routes, and added static file handling. introduced new components for improved user interaction and updated styles for better ui.
    
    ###

--- SetPlayList.Api/Program.cs
+++ SetPlayList.Api/Program.cs
@@ -2,6 +2,8 @@ using SetPlayList.Api.Clients;
 using SetPlayList.Api.Configuration;
 using SetPlayList.Api.Services;
 using SetPlayList.Core.Interfaces;
+using SetPlayList.Api.Components;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +13,15 @@ builder.Services.AddHttpClient<ISpotifyApiClient, SpotifyApiClient>();
 builder.Services.AddScoped<ISpotifyAuthService, SpotifyAuthService>();
 builder.Services.AddScoped<ISetlistFmApiClient, SetlistFmApiClient>();
 builder.Services.AddScoped<ISpotifyPlaylistService, SpotifyPlaylistService>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
 
 app.MapGet("/auth/spotify/login", (HttpContext context, ISpotifyAuthService spotifyAuthService) =>
 {
@@ -32,18 +41,20 @@ app.MapGet("/auth/spotify/callback", async (HttpContext context, ISpotifyAuthSer
     return success ? Results.Ok("Token saved.") : Results.Problem("Failed to exchange code for token.");
 });
 
-app.MapGet("/setlist/{setlistId}", async (ISetlistFmApiClient setlistFmApiClient, string setlistId) =>
+app.MapGet("api/setlist/{setlistId}", async (ISetlistFmApiClient setlistFmApiClient, string setlistId) =>
 {
-    var setlist = await setlistFmApiClient.GetSetlistAsync(setlistId);
+    var (setlist, httpStatusCode) = await setlistFmApiClient.GetSetlistAsync(setlistId);
 
     return Results.Ok(setlist);
 });
 
-app.MapGet("/playlist-preview/{setlistId}", async (ISpotifyPlaylistService spotifyPlaylistService, ISpotifyAuthService spotifyAuthService, string setlistId, HttpContext httpContext) =>
+app.MapGet("api/playlist-preview/{setlistId}", async (ISpotifyPlaylistService spotifyPlaylistService, ISpotifyAuthService spotifyAuthService, string setlistId, HttpContext httpContext) =>
 {
     var accessToken = await spotifyAuthService.GetCurrentAccessTokenAsync(httpContext);
     var result = await spotifyPlaylistService.GeneratePreviewAsync(setlistId, accessToken);
     return result.ProposedPlaylist;
 });
+
+app.MapRazorComponents<App>();
 
 app.Run();
